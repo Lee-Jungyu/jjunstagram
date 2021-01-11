1b5b35fdbe720e97e8658145efe1aceb0d2c74a5
Lee-Jungyu <jungyu09@gmail.com>
2021-01-11 20:52:05 +0900

    Connect React client to GraphQL server by Apollo

--- src/App.js
+++ src/App.js
@@ -2,24 +2,25 @@ import React, { Component } from 'react';
 import './App.css';
 import Header from './components/Header';
 import Post from './components/Post';
+import ApolloClient from 'apollo-boost';
+
+// connect react client to graphQL server
+const client = new ApolloClient({
+	uri: "http://localhost:4000"
+})
 
 class App extends Component {
 	render() {
-		return <div className="App">
-				<Header />
-				<section className="App-main">
-					<Post 
-						nickname="Chris" 
-						avatar="https://www.laravelnigeria.com/img/chris.jpg" 
-						caption="Moving the community!" 
-						image="https://pbs.twimg.com/media/DOXI0IEXkAAkokm.jpg" />
-					<Post 
-						nickname="OG" 
-						avatar="https://www.laravelnigeria.com/img/chris.jpg" 
-						caption="Holding a mic" 
-						image="https://pbs.twimg.com/media/DOXI0IEXkAAkokm.jpg" />
-				</section>
-			</div>;
+		return (
+			<ApolloProvider client={client}>
+				<div className="App">
+					<Header />
+					<section className="App-main">
+						<Post />
+					</section>
+				</div>
+			</ApolloProvider>
+		);
 	}
 }
 
