38a3e9a616384ec894c16a59d767cda7840c7a24
Lee-Jungyu <jungyu09@gmail.com>
2021-01-12 00:11:45 +0900

    Render server-provisioned data

--- src/App.js
+++ src/App.js
@@ -3,25 +3,24 @@ import './App.css';
 import Header from './components/Header';
 import Post from './components/Post';
 import ApolloClient from 'apollo-boost';
+import {ApolloProvider} from "react-apollo";
 
 // connect react client to graphQL server
 const client = new ApolloClient({
-	uri: "http://localhost:4000"
-})
+  	uri: "http://localhost:4000/graphql"
+});
 
-class App extends Component {
-	render() {
-		return (
-			<ApolloProvider client={client}>
-				<div className="App">
-					<Header />
-					<section className="App-main">
-						<Post />
-					</section>
-				</div>
-			</ApolloProvider>
-		);
-	}
-}
+const App = () => {
+	return (
+		<ApolloProvider client={client}>
+			<div className="App">
+				<Header />
+				<section className="App-main">
+					<Post />
+				</section>
+			</div>
+		</ApolloProvider>
+	);
+};
 
 export default App;
