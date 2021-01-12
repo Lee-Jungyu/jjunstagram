4815e4d7c63f6715cb9063e7ba28a34c5249c6e0
Lee-Jungyu <jungyu09@gmail.com>
2021-01-12 15:54:12 +0900

    Add keys.js

---
+++ keys.js
@@ -0,0 +1,11 @@
+const PUSHER_APP_ID = 'your app id';
+const PUSHER_APP_KEY = 'your app key';
+const PUSHER_APP_SECRET = 'your app secret';
+const PUSHER_CLUSTER = 'your cluster';
+
+module.exports = {
+    PUSHER_APP_ID,
+    PUSHER_APP_KEY,
+    PUSHER_APP_SECRET,
+    PUSHER_CLUSTER
+};
